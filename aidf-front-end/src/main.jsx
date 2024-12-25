0f397761e5c35be1c1852c3561dae52129d661f5
DharshanSR <dharshanravindran8@gmail.com>
2024-12-25 19:21:33 +0530

    Set up global styles in index.css and configure main.jsx for application initialization

--- aidf-front-end/src/main.jsx
+++ aidf-front-end/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import HomePage from "./pages/home/home.page.jsx";
+import JobPage from "./pages/job/job.page";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import RootLayout from "./layouts/root.layout.jsx";
+import SignInPage from "./pages/sign-in-page.jsx";
+import SignUpPage from "./pages/sign-up-page.jsx";
+import MainLayout from "./layouts/main.layout.jsx";
+
+const router = createBrowserRouter([
+    {
+        element: <RootLayout />,
+        children: [
+            {
+                element: <MainLayout />,
+                children: [
+                    {
+                        path: "/",
+                        element: <HomePage />,
+                    },
+                    {
+                        path: "/job/:id",
+                        element: <JobPage />,
+                    },
+                ],
+            },
+            {
+                path: "/sign-in",
+                element: <SignInPage />,
+            },
+            {
+                path: "/sign-up",
+                element: <SignUpPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
