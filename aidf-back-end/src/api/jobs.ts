196de346907a84e3c26121718d9de65430504ff0
DharshanSR <dharshanravindran8@gmail.com>
2024-12-27 16:47:06 +0530

    Refactor jobs router: Remove duplicate imports and redundant routes

--- aidf-back-end/src/api/jobs.ts
+++ aidf-back-end/src/api/jobs.ts
@@ -1,9 +1,19 @@
 import express from "express";
-import {createJob, deleteJob, getAllJobs, getJobById, updateJob} from "../application/jobs";
+import {
+    createJob,
+    deleteJob,
+    getAllJobs,
+    getJobById,
+    updateJob
+} from "../application/jobs"; // Update the path according to your file structure
 
 const jobsRouter = express.Router();
 
+// Routes for the jobs resource
 jobsRouter.route("/").get(getAllJobs).post(createJob);
-jobsRouter.route("/:_id").get(getJobById).put(updateJob).delete(deleteJob);
+jobsRouter.route("/:_id")
+    .get(getJobById)
+    .put(updateJob)
+    .delete(deleteJob);
 
 export default jobsRouter;
