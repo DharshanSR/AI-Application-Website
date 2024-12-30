f471cf461d6f30198b7c0071991c6a3a96b2d022
DharshanSR <dharshanravindran8@gmail.com>
2024-12-30 17:06:05 +0530

    Set up Express server with job and job application routes
    - Set up the Express server with the following configurations:
      - Used `dotenv/config` to load environment variables from a `.env` file.
      - Imported and connected to the database using `connectDB()`.
      - Set up middleware for parsing JSON requests and enabling CORS.
      - Added job and job application routes with `jobsRouter` and `jobApplicationRouter` for handling job-related and job application-related requests.
      - The server listens on port 8000.
    - Ensured that the necessary routes are properly registered for handling job and job application operations.

--- aidf-back-end/src/index.ts
+++ aidf-back-end/src/index.ts
@@ -1,14 +1,19 @@
 import "dotenv/config";
-import { connectDB } from "./infrastructure/db";
 import express from "express";
 import jobsRouter from "./api/jobs";
+import { connectDB } from "./infrastructure/db";
+import jobApplicationRouter from "./api/jobApplication";
+import  cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 connectDB();
 
 app.use("/jobs", jobsRouter);
+
+app.use("/jobApplications",jobApplicationRouter)
 
 const PORT = 8000
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
